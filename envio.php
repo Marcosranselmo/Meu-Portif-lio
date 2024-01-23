e162ab49469536aec8e01794e28693511e9da88a
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2024-01-23 14:07:07 -0300

    form-contact-update

--- envio.php
+++ envio.php
@@ -1,47 +1,37 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
+    use PHPMailer\PHPMailer\PHPMailer;
+    
+    require './vendor/autoload.php';
+    
+    $mail = new PHPMailer;
+    
+    $mail->isSMTP();
+    //$mail->SMTPDebug = 2;
+    $mail->Host = 'smtp.hostinger.com';
+    $mail->Port = 587;
+    //$mail->ChartSet = 'utf8';
+    
+    $mail->SMTPAuth = true;
+    $mail->Username = 'contato@meuportifolio.tech';
+    $mail->Password = 'App.9977';
+    $mail->setFrom('contato@meuportifolio.tech', 'Your Name');
+    $mail->addReplyTo('contato@meuportifolio.tech', 'Your Name');
+    $mail->addAddress('contato@meuportifolio.tech', 'Receiver Name');
+    $mail->Subject = 'Meu Portifolio';
+    
+    $body = "Mensagem do site meuportifolio.tech
+    
+        Nome: ". $_POST['nome']. "
+        E-mail: ". $_POST['emai']. "
+        Assunto: ". $_POST['assunto']. "
+        Mensagem: ". $_POST['mensagem'];
 
-require './vendor/autoload.php';
-
-if (isset($_POST['enviar'])) {
-    $mail = new PHPMailer(true);
-
-    try {
-        //Configurações do servidor
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-        $mail->isSMTP();                      
-        $mail->Host = 'smtp.hostinger.com';
-        $mail->SMTPAuth = true;                             
-        $mail->Username = 'contato@meuportifolio.tech';        
-        $mail->Password = 'App.9977';                         
-        $mail->SMTPSecure = 'SSL/TLS'; 
-        $mail->SMTPSecure = 'PHPMailer::ENCRYPTION_SMTPS';                        
-        $mail->Port = 465;     
-
-        //Destinatario
-        $mail->setFrom('contato@meuportifolio.tech', 'Mailer');
-        $mail->addAddress('contato@meuportifolio.tech', 'AddAddress01'); 
-        $mail->addReplyTo('contato@meuportifolio.tech', 'Informações');
-        $mail->isHTML(true);                   
-        $mail->Subject = 'MSN-MeuPortifolio.tch';
-
-        $body = "mensagem enviada através do site, segue informações
-        abaixo:<br>
-            Nome: ". $_POST['nome']. "<br>
-            E-mail: ". $_POST['emai']. "<br>
-            Assunto: ". $_POST['assunto']. "<br>
-            Mensagem:<br> ". $_POST['mensagem'];
-
-        $mail->Body = $body;
-
-        $mail->send();
-        echo 'E-mail enviado com sucesso!';
-    } catch (Exception $e) {
-        echo 'Erro ao enviar o e-mail!';
-        echo 'Qual é o erro que tivemos: ' . $mail->ErrorInfo;
+    $mail->Body = $body;
+            
+    //$mail->addAttachment('test.txt');
+    if (!$mail->send()) {
+        echo 'Erro: Mensagem não enviada, tente pelo whatsApp: 1196497-9977. Desculpe-nos! ' . $mail->ErrorInfo;
+    } else {
+        echo 'Sua mensagem foi enviada com sucesso!';
     }
-}else{
-    echo "Erro ao enviar e-mail, acesso não foi via formulário!";
-}
+?>
--- vendor/autoload.php
+++ vendor/autoload.php
@@ -20,6 +20,6 @@ if (PHP_VERSION_ID < 50600) {
     );
 }
 
-require_once __DIR__ . '/composer/autoload_real.php';
+require_once __DIR__ . 'composer/autoload_real.php';
 
 return ComposerAutoloaderInit2185d2f99bcd56787481d9357a5972d3::getLoader();
