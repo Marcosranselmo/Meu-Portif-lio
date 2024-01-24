27b77af148434120e9ff241b98e8c1c817c3717a
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2024-01-24 14:26:50 -0300

    contact-retorn

--- envio.php
+++ envio.php
@@ -5,33 +5,34 @@
     
     $mail = new PHPMailer;
     
-    $mail->isSMTP();
-    //$mail->SMTPDebug = 2;
-    $mail->Host = 'smtp.hostinger.com';
-    $mail->Port = 587;
-    //$mail->ChartSet = 'utf8';
+    try {
     
-    $mail->SMTPAuth = true;
-    $mail->Username = 'contato@meuportifolio.tech';
-    $mail->Password = 'App.9977';
-    $mail->setFrom('contato@meuportifolio.tech', 'Your Name');
-    $mail->addReplyTo('contato@meuportifolio.tech', 'Your Name');
-    $mail->addAddress('contato@meuportifolio.tech', 'Receiver Name');
-    $mail->Subject = 'Meu Portifolio';
+        $mail->isSMTP();
+        //$mail->SMTPDebug = 2;
+        $mail->Host = 'smtp.hostinger.com';
+        $mail->Port = 587;
+        $mail->ChartSet = 'utf8';
+        
+        $mail->SMTPAuth = true;
+        $mail->Username = 'contato@meuportifolio.tech';
+        $mail->Password = 'App.9977';
+        $mail->setFrom('contato@meuportifolio.tech');
+        $mail->addReplyTo('contato@meuportifolio.tech');
+        $mail->addAddress('contato@meuportifolio.tech');
+        $mail->Subject = 'Meu Portifolio';
+        
+        $body = "Mensagem do site meuportifolio.tech
     
-    $body = "Mensagem do site meuportifolio.tech
-    
-        Nome: ". $_POST['nome']. "
-        E-mail: ". $_POST['emai']. "
-        Assunto: ". $_POST['assunto']. "
-        Mensagem: ". $_POST['mensagem'];
+            Nome: ". $_POST['nome']. "
+            E-mail: ". $_POST['emai']. "
+            Assunto: ". $_POST['assunto']. "
+            Mensagem: ". $_POST['mensagem'];
 
-    $mail->Body = $body;
+        $mail->Body = $body;
             
-    //$mail->addAttachment('test.txt');
-    if (!$mail->send()) {
-        echo 'Erro: Mensagem não enviada, tente pelo whatsApp: 1196497-9977. Desculpe-nos! ' . $mail->ErrorInfo;
-    } else {
-        echo 'Sua mensagem foi enviada com sucesso!';
+        $mail->send(); //{
+        header("location:retorno.php");
+    } catch (Exception $e) {
+        echo "Erro: {$mail->ErrorInfo}";
     }
 ?>
---
+++ retorno.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    
+    <title>Retorno</title>
+</head>
+<body class="row justify-content-center" style="background-color:#12444b">
+    <div class="col-md-4 mt-5">
+        <h5 class="text-center mt-5" style="color:#ffffff">Obrigado por entrar em contato!!</h5>
+        <div class="alert alert-success text-center mt-4">
+        <strong>Mensagem enviada com sucesso! Aguardem nosso retorno!</strong>
+        </div>
+        <div class="alert alert-success text-center mt-4">
+        <strong><a href="/">Voltar</a></strong>
+        </div>
+    </div>
+</body>
+</html>
